0c62b9cbcb9f4c35086edc94b69e6c1e8c4b660f
abwrackpraemie <stemmer.markus@web.de>
2015-08-11 16:07:21 +0200

    zweite Vokabelliste hinzugefügt

--- src/vocab.h
+++ src/vocab.h
@@ -31,4 +31,31 @@ char c3_list1[4][23][25] =
     }
   };
 
+char c3_list2[4][28][25] =
+  {
+    {
+      {0},{1},{2},{3},{4},{5},{6},{7},{8},{9},
+      {10},{11},{12},{13},{14},{15},{16},{17},{18},{19},
+      {20},{21},{22},{23},{24},{25},{26},{27}
+    },
+    {
+      "guó","lǎoshī","nà","nǎ","rén","shéi","shì","Zhōngguó","nǎinai","nín",
+      "wàipó","wàiyǔ","yīshēng","zhè","gāoxìng","guìxìng","jiào","jìn","jìnlai","jìzhě",
+      "kěyǐ","lái","qǐng","qǐngwèn","rènshi","wèn","xiānsheng","xìng"
+    },
+    {
+      "\u56FD","\u8001\u5E08","\u90A3","\u54EA","\u4EBA","\u8C01","\u662F","\u4E2D\u56FD","\u5976\u5976","\u60A8",
+      "\u5916\u5A46","\u5916\u8BED","\u533B\u751F","\u8FD9","\u9AD8\u5174","\u8D35\u59D3","\u53EB","\u8FDB","\u8FDB\u6765","\u8BB0\u8005",
+      "\u53EF\u4EE5","\u6765","\u8BF7","\u8BF7\u95EE","\u8BA4\u8BC6","\u95EE","\u5148\u751F","\u59D3"
+      //"国","老师","那","哪","人","谁","是","中国","奶奶","您",
+      //"外婆","外语","医生","这","高兴","贵姓","叫","进","进来","记者",
+      //"可以","来","请","请问","认识","问","先生","姓"
+    },
+    {
+      "Land","Lehrer","das (da/dort)","welche","Mensch","wer","sein","China","Grossm. vaeterl.","Sie",
+      "Grossm. muetterl.","Fremdsprache","Arzt","dies (hier)","glücklich","(höflich NN erfragen)","heißen","eintreten","hereinkommen","Journalist",
+      "können","kommen","bitten","darf ich fragen","jmd. kennen","fragen","Herr"
+    }
+  };
+
 #endif /* VOCAB_H */ 
